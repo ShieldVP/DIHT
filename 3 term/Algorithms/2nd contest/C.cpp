fb2484baa4247a9cbe71f136576aad7e66552c32
ShieldVP <yury2001@mail.ru>
2020-12-19 11:28:14 +0300

    Code style fix

--- 3 term/Algorithms/2nd contest/C.cpp
+++ 3 term/Algorithms/2nd contest/C.cpp
@@ -9,13 +9,14 @@
  Для каждого запроса выведите число с четырьмя знаками после запятой – расстояния до поверхности минимально возможного воображаемого яблока.
 */
 
-#include <cstdio>
+#include <iostream>
 #include <vector>
 #include <stack>
 #include <map>
 #include <limits>
 #include <algorithm>
 #include <cmath>
+#include <tuple>
 using namespace std;
 
 struct Point {
@@ -25,11 +26,16 @@ struct Point {
 };
 
 bool operator==(const Point& A, const Point& B) {
-    return A.x == B.x && A.y == B.y && A.z == B.z;
+    return std::tie(A.x, A.y, A.z) == std::tie(B.x, B.y, B.z);
 }
 
 bool operator<(const Point& A, const Point& B) {
-    return (A.z < B.z) || (A.z == B.z && A.x < B.x) || (A.z == B.z && A.x == B.x && A.y < B.y);
+    return std::tie(A.z, A.x, A.y) < std::tie(B.z, B.x, B.y);
+}
+
+std::istream& operator>>(std::istream& stream, Point& A) {
+    stream >> A.x >> A.y >> A.z;
+    return stream;
 }
 
 struct Vector {
@@ -37,8 +43,16 @@ struct Vector {
     double norma = -1;
 
     Vector() = default;
-    Vector(long long _x, long long _y, long long _z) : x(_x), y(_y), z(_z) {}
-    Vector(const Point& A, const Point& B) : x(B.x - A.x), y(B.y - A.y), z(B.z - A.z) {}
+    Vector(long long x, long long y, long long z) : 
+    x(x), 
+    y(y), 
+    z(z) 
+    {}
+    Vector(const Point& A, const Point& B) : 
+    x(B.x - A.x), 
+    y(B.y - A.y), 
+    z(B.z - A.z) 
+    {}
 
     double len() {
         if (norma >= 0) return norma;
@@ -46,53 +60,60 @@ struct Vector {
     }
 };
 
-Vector crossProduct(const Vector& A, const Vector& B) {
+static Vector crossProduct(const Vector& A, const Vector& B) {
     return Vector(A.y * B.z - A.z * B.y, A.z * B.x - A.x * B.z, A.x * B.y - A.y * B.x);
 }
 
-long long dotProduct(const Vector& A, const Vector& B) {
+static long long dotProduct(const Vector& A, const Vector& B) {
     return A.x * B.x + A.y * B.y + A.z * B.z;
 }
 
-bool relativeSignFlat(const Vector& normal, const Point& in, const Point& A) {
+static bool relativeSignFlat(const Vector& normal, const Point& in, const Point& A) {
     return dotProduct(normal, Vector(A, in)) > 0;
 }
 
 struct Edge {
     Point A, B, C;
 
-    Edge(const Point& _A, const Point& _B, const Point& _C) : A(_A), B(_B), C(_C) {}
+    Edge(const Point& A, const Point& B, const Point& C) : 
+    A(A), 
+    B(B), 
+    C(C) 
+    {}
 
     bool flatContains(const Point& D) const {
         return A == D || B == D || C == D;
     }
 };
 
-bool operator<(const Edge& A, const Edge& B) {
-    if (A.A < A.B) {
-        if (B.A < B.B) return A.A == B.A ? A.B < B.B : A.A < B.A;
-        else return A.A == B.B ? A.B < B.A : A.A < B.B;
-    }
-    else {
-        if (B.A < B.B) return A.B == B.A ? A.A < B.B : A.B < B.A;
-        else return A.B == B.B ? A.A < B.A : A.B < B.B;
-    }
+bool operator<(const Edge& first, const Edge& second) {
+    if (first.A < first.B) return second.A < second.B ? 
+               std::tie(first.A, first.B) < std::tie(second.A, second.B) : 
+               std::tie(first.A, first.B) < std::tie(second.B, second.A);
+    else return second.A < second.B ? 
+               std::tie(first.B, first.A) < std::tie(second.A, second.B) : 
+               std::tie(first.B, first.A) < std::tie(second.B, second.A);
 }
 
-bool operator==(const Edge& A, const Edge& B) {
-    return (A.A == B.A && A.B == B.B) || (A.B == B.A && A.A == B.B);
+bool operator==(const Edge& first, const Edge& second) {
+    return (first.A == second.A && first.B == second.B) ||
+           (first.B == second.A && first.A == second.B);
 }
 
 struct Face {
     Point A, B, C;
     Vector normal;
-    long long D;
 
-    Face(const Point& _A, const Point& _B, const Point& _C) : A(_A), B(_B), C(_C) {}
+    Face(const Point& A, const Point& B, const Point& C, const Vector& normal) : 
+    A(A), 
+    B(B), 
+    C(C),
+    normal(normal)
+    {}
 };
 
-double flatDistance(Face& face, const Point& A) {
-    return abs(dotProduct(face.normal, Vector(A, face.A))) / face.normal.len();
+static double flatDistance(Face& face, const Point& point) {
+    return abs(dotProduct(face.normal, Vector(point, face.A))) / face.normal.len();
 }
 
 class PointsSet {
@@ -100,42 +121,9 @@ public:
     PointsSet(std::vector<Point>  arr) : array(std::move(arr)) {}
 
     void computeConvexHull() {
-        // searching for the first edge in convex hull
-        auto firstPoint = array.front(), secondPoint = firstPoint;
-        for (auto& point : array)
-            if (point < firstPoint)
-                firstPoint = point;
-        auto minTG = std::numeric_limits<double>::infinity();
-        for (auto& point : array) {
-            if (point.x == firstPoint.x && point.y == firstPoint.y) continue;
-            auto h = point.z - firstPoint.z;
-            auto l = std::sqrt(pow(point.x - firstPoint.x, 2) + pow(point.y - firstPoint.y, 2));
-            auto newTG = h / l;
-            if (newTG < minTG) {
-                secondPoint = point;
-                minTG = newTG;
-            }
-        }
-        size_t j = 0;
-        while (array[j] == firstPoint || array[j] == secondPoint) ++j;
-        auto thirdPoint = array[j++];
-        while (array[j] == firstPoint || array[j] == secondPoint) ++j;
-        auto basePoint = array[j];
-        auto firstNormal = crossProduct(Vector(thirdPoint, firstPoint), Vector(thirdPoint, secondPoint));
-        auto firstSign = relativeSignFlat(firstNormal, firstPoint, basePoint);
-        for (size_t point = j + 1; point < array.size(); ++point) {
-            if (array[point] == firstPoint || array[point] == secondPoint) continue;
-            if (relativeSignFlat(firstNormal, firstPoint, array[point]) != firstSign) {
-                thirdPoint = array[point];
-                firstNormal = crossProduct(Vector(thirdPoint, firstPoint), Vector(thirdPoint, secondPoint));
-                firstSign = relativeSignFlat(firstNormal, firstPoint, basePoint);
-            }
-        }
-
         std::stack<Edge> edges;
         std::map<Edge, size_t> used;
-        edges.emplace(firstPoint, secondPoint, thirdPoint);
-        //++used[edges.top()];
+        edges.emplace(getFirstEdge());
         while (!edges.empty()) {
             auto edge = edges.top();
             edges.pop();
@@ -156,9 +144,7 @@ public:
                 }
             }
 
-            hull.emplace_back(edge.A, edge.B, array[newPoint]);
-            hull.back().normal = newNormal;
-            hull.back().D = -dotProduct(newNormal, Vector(edge.A.x, edge.A.y, edge.A.z));
+            hull.emplace_back(edge.A, edge.B, array[newPoint], newNormal);
             edges.emplace(edge.A, array[newPoint], edge.B);
             ++used[edges.top()];
             edges.emplace(edge.B, array[newPoint], edge.A);
@@ -166,38 +152,67 @@ public:
         }
     }
 
-    double distanceToHull(const Point& A) {
+    double distanceToHull(const Point& point) {
         auto minDist = std::numeric_limits<double>::infinity();
         for (auto& face : hull)
-            minDist = std::min(flatDistance(face, A), minDist);
+            minDist = std::min(flatDistance(face, point), minDist);
         return minDist;
-    }
-
-    void print() const {
-        for (auto& face : hull)
-            printf("%lf %lf %lf %lf\n", face.normal.x / (double)face.D, face.normal.y / (double)face.D, face.normal.z / (double)face.D, 1.0);
     }
 
 private:
     std::vector<Point> array;
     std::vector<Face> hull;
+
+    Edge getFirstEdge() const {
+        auto firstPoint = array.front(), secondPoint = firstPoint;
+        for (auto& point : array)
+            if (point < firstPoint)
+                firstPoint = point;
+        auto minTan = std::numeric_limits<double>::infinity();
+        for (auto& point : array) {
+            if (point.x == firstPoint.x && point.y == firstPoint.y) continue;
+            auto h = point.z - firstPoint.z;
+            auto l = std::sqrt(pow(point.x - firstPoint.x, 2) + pow(point.y - firstPoint.y, 2));
+            auto newTan = h / l;
+            if (newTan < minTan) {
+                secondPoint = point;
+                minTan = newTan;
+            }
+        }
+        size_t j = 0;
+        while (array[j] == firstPoint || array[j] == secondPoint) ++j;
+        auto thirdPoint = array[j++];
+        while (array[j] == firstPoint || array[j] == secondPoint) ++j;
+        auto basePoint = array[j];
+        auto firstNormal = crossProduct(Vector(thirdPoint, firstPoint), Vector(thirdPoint, secondPoint));
+        auto firstSign = relativeSignFlat(firstNormal, firstPoint, basePoint);
+        for (size_t point = j + 1; point < array.size(); ++point) {
+            if (array[point] == firstPoint || array[point] == secondPoint) continue;
+            if (relativeSignFlat(firstNormal, firstPoint, array[point]) != firstSign) {
+                thirdPoint = array[point];
+                firstNormal = crossProduct(Vector(thirdPoint, firstPoint), Vector(thirdPoint, secondPoint));
+                firstSign = relativeSignFlat(firstNormal, firstPoint, basePoint);
+            }
+        }
+        return Edge(firstPoint, secondPoint, thirdPoint);
+    }
 };
 
 int main() {
     size_t N, M;
-    scanf("%lu", &N);
+    std::cin >> N
     std::vector<Point> arr(N);
     for (auto& point : arr)
-        scanf("%lld %lld %lld", &point.x, &point.y, &point.z);
+        std::cin >> point;
 
     PointsSet set = std::move(arr);
     set.computeConvexHull();
 
-    scanf("%lu", &M);
+    std::cin >> M;
     Point point;
     for (size_t i = 0; i < M; ++i) {
-        scanf("%lld %lld %lld", &point.x, &point.y, &point.z);
-        printf("%0.4lf\n", set.distanceToHull(point));
+        std::cin >> point;
+        std::cout << std::setprecision(4) << set.distanceToHull(point);
     }
     return 0;
 }

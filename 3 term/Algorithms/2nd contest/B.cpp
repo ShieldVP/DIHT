cb2f60190961e8e170a2374300504f7d39e065f8
ShieldVP <yury2001@mail.ru>
2020-12-21 19:02:59 +0300

    Code style fix

--- 3 term/Algorithms/2nd contest/B.cpp
+++ 3 term/Algorithms/2nd contest/B.cpp
@@ -7,17 +7,12 @@
 struct Point {
     int64_t x = 0, y = 0;
 
-    Point() {
-        read();
-    }
+    Point() = default;
 
-    Point(int64_t x, int64_t y) : x(x), y(y) {}
-
-    void read() {
-        int64_t _x, _y;
-        scanf("%ld%ld", &x, &y);
-        x = _x, y = _y;
-    }
+    Point(int64_t x, int64_t y) :
+    x(x),
+    y(y)
+    {}
 };
 
 bool operator==(const Point& A, const Point& B) {
@@ -25,59 +20,78 @@ bool operator==(const Point& A, const Point& B) {
 }
 
 bool operator!=(const Point& A, const Point& B) {
-    return A.x != B.x || A.y != B.y;
+    return !(A == B);
+}
+
+std::istream& operator>>(std::istream& stream, Point& A) {
+    stream >> A.x >> A.y >> A.z;
+    return stream;
 }
 
 class PointsSet {
 public:
-    explicit PointsSet(size_t size) {
-        set.resize(size, Point(0, 0));
-        for (size_t i = 0; i < size; ++i)
-            set[i] = Point();
-    }
+    explicit PointsSet(size_t size);
 
-    double convexHullPerimeter() {
-        size_t rightBottom = 0;
-        for (size_t i = 1; i < set.size(); ++i)
-            if (set[i].y < set[rightBottom].y || (set[i].y == set[rightBottom].y && set[i].x > set[rightBottom].x))
-                rightBottom = i;
-        std::swap(set[0], set[rightBottom]);
-        std::sort(set.begin() + 1, set.end(), [&](const Point& A, const Point& B) {
-            if (B.x == A.x && B.y == A.y) return true;
-            auto cross = crossProductZ(A, set.front(), B);
-            if (cross == 0) return dist(set.front(), A) > dist(set.front(), B);
-            return cross > 0;
-        });
-        auto last = std::unique(set.begin(), set.end());
-        set.erase(last, set.end());
-        size_t size = 1;
-        for (size_t i = 2; i < set.size(); ++i) {
-            while (size > 0 && crossProductZ(set[size - 1], set[size], set[i]) > 0)
-                --size;
-            std::swap(set[i], set[++size]);
-        }
-        double perimeter = dist(set[0], set[size]);
-        for (size_t i = 0; i < size; ++i)
-            perimeter += dist(set[i], set[i + 1]);
-        return perimeter;
-    }
+    void buildConvexHull();
+
+    double convexHullPerimeter() const;
 
 private:
-    double dist(const Point& A, const Point& B) const {
-        return std::sqrt((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y));
-    }
+    double dist(const Point& A, const Point& B) const;
 
-    int64_t crossProductZ(const Point& A, const Point& B, const Point& C) const {
-        return (A.x - B.x) * (C.y - B.y) - (C.x - B.x) * (A.y - B.y);  // BA x BC
-    }
+    int64_t crossProductZ(const Point& A, const Point& B, const Point& C) const;
 
-    std::vector<Point> set;
+    std::vector<Point> points;
 };
+
+explicit PointsSet::PointsSet(size_t size) {
+    points.resize(size);
+    for (size_t i = 0; i < size; ++i)
+        std::cin >> points[i];
+}
+
+void PointsSet::buildConvexHull() {
+    size_t rightBottom = 0;
+    for (size_t i = 1; i < points.size(); ++i)
+        if (points[i].y < points[rightBottom].y || (points[i].y == points[rightBottom].y && points[i].x > points[rightBottom].x))
+            rightBottom = i;
+    std::swap(points[0], points[rightBottom]);
+    std::sort(points.begin() + 1, points.end(), [&](const Point& A, const Point& B) {
+        if (B.x == A.x && B.y == A.y) return true;
+        auto cross = crossProductZ(A, points.front(), B);
+        if (cross == 0) return dist(points.front(), A) > dist(points.front(), B);
+        return cross > 0;
+    });
+    auto last = std::unique(points.begin(), points.end());
+    points.erase(last, points.end());
+    size_t size = 1;
+    for (size_t i = 2; i < points.size(); ++i) {
+        while (size > 0 && crossProductZ(points[size - 1], points[size], points[i]) > 0)
+            --size;
+        std::swap(points[i], points[++size]);
+    }
+}
+
+double PointsSet::convexHullPerimeter() const {
+    double perimeter = dist(points[0], points[size]);
+    for (size_t i = 0; i < size; ++i)
+        perimeter += dist(points[i], points[i + 1]);
+    return perimeter;
+}
+
+double PointsSet::dist(const Point& A, const Point& B) const {
+    return std::sqrt((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y));
+}
+
+int64_t PointsSet::crossProductZ(const Point& A, const Point& B, const Point& C) const {
+    return (A.x - B.x) * (C.y - B.y) - (C.x - B.x) * (A.y - B.y);  // BA x BC
+}
 
 int main() {
     size_t N;
-    scanf("%zd", &N);
-    PointsSet set(N);
-    printf("%f", set.convexHullPerimeter());
+    std::cin >> N
+    PointsSet points(N);
+    points.buildConvexHull();
+    std::cout << points.convexHullPerimeter();
     return 0;
 }
